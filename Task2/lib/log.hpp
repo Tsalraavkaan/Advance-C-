85021b6c39588ab09c25bb14142afa0caf775615
Andrej <72415904+Tsalraavkaan@users.noreply.github.com>
2020-10-14 01:53:44 +0300

    Add files via upload

--- Task2/lib/log.hpp
+++ Task2/lib/log.hpp
@@ -6,24 +6,24 @@
 
 namespace Task2 {
 
-    class Logger {
-    private:
-        std::unique_ptr<BaseLogger> global_logger_;
-        Logger();
-        ~Logger() = default;
-    public:
-        static Logger &get_instance();
-        BaseLogger &get_global_logger();
-        void set_global_logger(std::unique_ptr<BaseLogger>);
-    };
+class Logger {
+private:
+    std::unique_ptr<BaseLogger> global_logger_;
+    Logger();
+    ~Logger() = default;
+public:
+    static Logger &get_instance();
+    BaseLogger &get_global_logger();
+    void set_global_logger(std::unique_ptr<BaseLogger>);
+};
 
-    std::unique_ptr<FileLogger> init_with_file_logger(const std::string &path, const Level level);
-    std::unique_ptr<StdoutLogger> init_with_stdout_logger(const Level level);
-    std::unique_ptr<StderrLogger> init_with_stderr_logger(const Level level);
-    void debug(const std::string &);
-    void info(const std::string &);
-    void warn(const std::string &);
-    void error(const std::string &);
-}
+std::unique_ptr<FileLogger> init_with_file_logger(const std::string &path, const Level level);
+std::unique_ptr<StdoutLogger> init_with_stdout_logger(const Level level);
+std::unique_ptr<StderrLogger> init_with_stderr_logger(const Level level);
+void debug(const std::string &);
+void info(const std::string &);
+void warn(const std::string &);
+void error(const std::string &);
+} //namespace Task2
 
 #endif
--- Task2/src/log.cpp
+++ Task2/src/log.cpp
@@ -1,49 +1,50 @@
 #include "log.hpp"
 
 namespace Task2 {
-    Logger::Logger() : global_logger_(std::unique_ptr<StdoutLogger>(new StdoutLogger())) {}
 
-    Logger &Logger::get_instance() {
-        static Logger log_instance;
-        return log_instance;
-    }
+Logger::Logger() : global_logger_(std::unique_ptr<StdoutLogger>(new StdoutLogger())) {}
 
-    BaseLogger &Logger::get_global_logger() {
-        return *global_logger_;
-    }
-
-    void Logger::set_global_logger(std::unique_ptr<BaseLogger> logger) {
-        if (logger != nullptr) {
-            global_logger_ = std::move(logger);
-        }
-    }
-
-    std::unique_ptr<FileLogger> init_with_file_logger(const std::string &path, const Level level) {
-        return std::make_unique<FileLogger>(path, level);
-    }
-
-    std::unique_ptr<StdoutLogger> init_with_stdout_logger(const Level level) {
-        return std::make_unique<StdoutLogger>(level);
-    }
-
-    std::unique_ptr<StderrLogger> init_with_stderr_logger(const Level level) {
-        return std::make_unique<StderrLogger>(level);
-    }
-
-    void debug(const std::string &message) {
-        Logger::get_instance().get_global_logger().debug(message);
-    }
-
-    void info(const std::string &message) {
-        Logger::get_instance().get_global_logger().info(message);
-    }
-
-    void warn(const std::string &message) {
-        Logger::get_instance().get_global_logger().warn(message);
-    }
-
-    void error(const std::string &message) {
-        Logger::get_instance().get_global_logger().error(message);
-    }
-
+Logger &Logger::get_instance() {
+    static Logger log_instance;
+    return log_instance;
 }
+
+BaseLogger &Logger::get_global_logger() {
+    return *global_logger_;
+}
+
+void Logger::set_global_logger(std::unique_ptr<BaseLogger> logger) {
+    if (logger != nullptr) {
+        global_logger_ = std::move(logger);
+    }
+}
+
+std::unique_ptr<FileLogger> init_with_file_logger(const std::string &path, const Level level) {
+    return std::make_unique<FileLogger>(path, level);
+}
+
+std::unique_ptr<StdoutLogger> init_with_stdout_logger(const Level level) {
+    return std::make_unique<StdoutLogger>(level);
+}
+
+std::unique_ptr<StderrLogger> init_with_stderr_logger(const Level level) {
+    return std::make_unique<StderrLogger>(level);
+}
+
+void debug(const std::string &message) {
+    Logger::get_instance().get_global_logger().debug(message);
+}
+
+void info(const std::string &message) {
+    Logger::get_instance().get_global_logger().info(message);
+}
+
+void warn(const std::string &message) {
+    Logger::get_instance().get_global_logger().warn(message);
+}
+
+void error(const std::string &message) {
+    Logger::get_instance().get_global_logger().error(message);
+}
+
+} //namespace Task2
